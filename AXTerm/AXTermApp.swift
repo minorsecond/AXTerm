2ff36b0ebd44253c39f43bd52ad4dc8660cb3bd3
Ross Wardrup <minorsecond@gmail.com>
2026-01-28 13:01:47 -0700

    Initial Commit

---
+++ AXTerm/AXTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXTermApp.swift
+//  AXTerm
+//
+//  Created by Ross Wardrup on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct AXTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
